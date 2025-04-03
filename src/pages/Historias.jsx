d7ceb84a39a800b256877c0afcdc9f2d97472850
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2025-04-03 13:49:53 -0300

    feat: add new page components for Game, Historias, Mapa, and Testes

---
+++ src/pages/Historias.jsx
@@ -0,0 +1,5 @@
+function Historias() {
+    return (<h1>Historias</h1> );
+}
+
+export default Historias;
---
+++ src/pages/Testes.jsx
@@ -0,0 +1,5 @@
+function Testes() {
+    return (<h1>Testes</h1> );
+}
+
+export default Testes;
