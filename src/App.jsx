1875f65a74cd66501e39a8391a1c6f5a4a5e69c5
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2025-04-03 13:36:21 -0300

    feat: update App component structure and add Tailwind CSS support

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import { Outlet } from "react-router-dom";
-
 
 export default function App() {
   return (
-    <div>
-      <ScrollToTop />
+    <>
       <Outlet />
-    </div>
+    </>
   );
 }
