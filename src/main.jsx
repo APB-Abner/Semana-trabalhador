907c1d9d7b5c78d8ef0ceee1344436521d550010
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2025-04-03 13:29:42 -0300

    refactor: remove App.css and update App component for routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
 import App from './App.jsx'
 
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Scene /> },
+      // { path: '/story', element: <Story /> },
+      // { path: '/legendary', element: <Legendary /> }
+      // { path: '*', element: <PageNotFound /> },
+      // { path: ':name', element: <ContentPage /> }
+    ]
+  }
+])
+
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
+
