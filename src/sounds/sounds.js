4edc849c3840c36314f3a7b165c0ae149b6f3a21
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2025-04-10 09:28:32 -0300

    feat: add sound files and update sounds.js for enhanced audio feedback

--- src/sounds/sounds.js
+++ src/sounds/sounds.js
@@ -1,5 +1,8 @@
 import { Howl } from 'howler';
+import Acerto from './acerto.mp3';
+import Erro from './erro.mp3';
+import Vitoria from './vitoria.mp3';
 
-export const somAcerto = new Howl({ src: ['/sons/acerto.mp3'] });
-export const somErro = new Howl({ src: ['/sons/erro.mp3'] });
-export const somVitoria = new Howl({ src: ['/sons/vitoria.mp3'] });
+export const somAcerto = new Howl({ src: [Acerto] });
+export const somErro = new Howl({ src: [Erro] });
+export const somVitoria = new Howl({ src: [Vitoria] });
